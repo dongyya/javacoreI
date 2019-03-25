f9a6048f19af50f69d2ffe26ca2ad5c80ffdd04d
董雨艳 <dongyya@yonyou.com>
2019-03-25 20:54:06 +0800

    dele

--- javacoreI/src/interfaceAndinnerClass/innerClass/InnerClassTest.java
+++ javacoreI/src/interfaceAndinnerClass/innerClass/InnerClassTest.java
@@ -1,6 +1,12 @@
 package interfaceAndinnerClass.innerClass;
 
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class InnerClassTest {
 	
@@ -9,6 +15,35 @@ public class InnerClassTest {
 		clock.start();
 		JOptionPane.showMessageDialog(null, "Quit program?");
 		System.exit(0);
+	}
+}
+	
+class TalkingClock{
+	private int interval;
+	private boolean beep;
+	public TalkingClock(int interval,boolean beep){
+		this.interval = interval;
+		this.beep = beep;
+	}
+	
+	public void start(){
+		TimePrinter t = new TimePrinter();
+		Timer timer = new Timer(interval,t);
+		timer.start();
+	}
+	
+	public class TimePrinter implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Date now = new Date();
+			System.out.println("At this ton,the time is " + now);
+			if(beep){
+				Toolkit.getDefaultToolkit().beep();
+			}
+		}
+		
 	}
 
 }
